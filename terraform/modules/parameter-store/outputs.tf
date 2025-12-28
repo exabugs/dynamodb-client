f05e2a5dd17a6e15c8704d77b864db335f095b36
exabugs <exabugs@gmail.com>
2025-12-28 22:10:45 +0900

    fix: remove deleted Cognito parameters from outputs.tf
    - Remove cognito_user_pool_id, cognito_client_id, cognito_domain from outputs
    - These parameters were removed in v0.8.0 for library generalization
    - Fixes Terraform validation errors in consuming projects

--- terraform/modules/parameter-store/outputs.tf
+++ terraform/modules/parameter-store/outputs.tf
@@ -5,9 +5,6 @@ output "parameter_arns" {
   description = "作成されたParameter StoreパラメータのARN一覧"
   value = {
     records_api_url      = aws_ssm_parameter.app_records_api_url.arn
-    cognito_user_pool_id = aws_ssm_parameter.app_admin_ui_cognito_user_pool_id.arn
-    cognito_client_id    = aws_ssm_parameter.app_admin_ui_cognito_client_id.arn
-    cognito_domain       = aws_ssm_parameter.app_admin_ui_cognito_domain.arn
     dynamodb_table_name  = aws_ssm_parameter.infra_dynamodb_table_name.arn
     records_function_arn = aws_ssm_parameter.lambda_records_function_arn.arn
   }
@@ -18,9 +15,6 @@ output "parameter_names" {
   description = "作成されたParameter Storeパラメータの名前一覧"
   value = {
     records_api_url      = aws_ssm_parameter.app_records_api_url.name
-    cognito_user_pool_id = aws_ssm_parameter.app_admin_ui_cognito_user_pool_id.name
-    cognito_client_id    = aws_ssm_parameter.app_admin_ui_cognito_client_id.name
-    cognito_domain       = aws_ssm_parameter.app_admin_ui_cognito_domain.name
     dynamodb_table_name  = aws_ssm_parameter.infra_dynamodb_table_name.name
     records_function_arn = aws_ssm_parameter.lambda_records_function_arn.name
   }
@@ -31,9 +25,6 @@ output "parameter_paths" {
   description = "作成されたParameter Storeパラメータのパス一覧"
   value = {
     records_api_url      = aws_ssm_parameter.app_records_api_url.name
-    cognito_user_pool_id = aws_ssm_parameter.app_admin_ui_cognito_user_pool_id.name
-    cognito_client_id    = aws_ssm_parameter.app_admin_ui_cognito_client_id.name
-    cognito_domain       = aws_ssm_parameter.app_admin_ui_cognito_domain.name
     dynamodb_table_name  = aws_ssm_parameter.infra_dynamodb_table_name.name
     records_function_arn = aws_ssm_parameter.lambda_records_function_arn.name
   }
